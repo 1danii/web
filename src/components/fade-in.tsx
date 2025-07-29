db665e92fca2aa5b2c9eb73cebd4f24a128a9506
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2025-07-29 12:07:06 +0200

    fade-in blur, description

--- src/components/fade-in.tsx
+++ src/components/fade-in.tsx
@@ -5,12 +5,12 @@ export function FadeIn(props: PropsWithChildren<{ delay?: number }>) {
   return (
     <motion.div
       transition={{
-        duration: 0.4,
+        duration: 0.6,
         ease: "easeOut",
         delay: props.delay,
       }}
-      initial={{ y: -4, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: 8, opacity: 0, filter: "blur(6px)" }}
+      animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
     >
       {props.children}
     </motion.div>
