2959811337ec4632e70d12cf47fb27702d3050f9
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2026-01-10 01:05:21 +0100

    shader test

---
+++ src/components/shader.tsx
@@ -0,0 +1,23 @@
+/** @paper-design/shaders-react@0.0.68 */
+import { Dithering } from "@paper-design/shaders-react";
+
+/**
+ * Code exported from Paper
+ * https://app.paper.design/file/01K4QT299EY7536B6B6R6DAQ1K?node=01KEJG7DSJTBDDVSGGH6RZRMMC
+ * on Jan 10, 2026 at 12:19 AM.
+ */
+export function Shader() {
+  return (
+    <Dithering
+      speed={0.62}
+      shape="warp"
+      type="4x4"
+      size={1.5}
+      scale={0.63}
+      frame={70738.72900009489}
+      colorBack="#00000000"
+      colorFront="#BAFF29"
+      className="outline-muted/10 mt-8 aspect-3/1 w-full rounded-lg bg-clip-padding outline"
+    />
+  );
+}
