94c0f9c7108c0613b7e5cebe8bb749086df0ec8d
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2026-01-10 17:00:36 +0100

    better components page

--- src/components/examples/loading-button.tsx
+++ src/components/examples/loading-button.tsx
@@ -32,60 +32,61 @@ export function LoadingButton({
   }
 
   return (
-    <MotionButton
-      onClick={handleClick}
-      disabled={isLoaderAnimating}
-      style={{
-        borderRadius: "9999px",
-      }}
-      layout="position"
-      {...props}
-    >
-      <motion.span layout>{children}</motion.span>
-
-      <motion.div
-        animate={isLoaderAnimating ? "visible" : "hidden"}
-        variants={{
-          visible: {
-            width: "1rem",
-            marginLeft: "0.5rem",
-          },
-          hidden: {
-            width: 0,
-            marginLeft: 0,
-            transition: {
-              delay: 0.1,
-            },
-          },
+    <div className="flex size-full items-center justify-center">
+      <MotionButton
+        onClick={handleClick}
+        disabled={isLoaderAnimating}
+        style={{
+          borderRadius: "9999px",
         }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
+        layout="position"
+        {...props}
       >
-        <AnimatePresence>
-          {isLoaderAnimating && (
-            <motion.svg
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 1,
-                transition: { delay: 0.3, ease: "easeInOut" },
-              }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.1 }}
-              className="size-4! animate-spin"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/10000/svg"
-            >
-              <path
-                className="fill-gray-200"
-                d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-              />
-              <path
-                className="fill-black"
-                d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
-              ></path>
-            </motion.svg>
-          )}
-        </AnimatePresence>
-      </motion.div>
-    </MotionButton>
+        <motion.span layout>{children}</motion.span>
+        <motion.div
+          animate={isLoaderAnimating ? "visible" : "hidden"}
+          variants={{
+            visible: {
+              width: "1rem",
+              marginLeft: "0.5rem",
+            },
+            hidden: {
+              width: 0,
+              marginLeft: 0,
+              transition: {
+                delay: 0.1,
+              },
+            },
+          }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <AnimatePresence>
+            {isLoaderAnimating && (
+              <motion.svg
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { delay: 0.3, ease: "easeInOut" },
+                }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+                className="size-4! animate-spin"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/10000/svg"
+              >
+                <path
+                  className="fill-gray-200"
+                  d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+                />
+                <path
+                  className="fill-black"
+                  d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+                ></path>
+              </motion.svg>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </MotionButton>
+    </div>
   );
 }
