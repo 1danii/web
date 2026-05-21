a1399b5af7a68eef04725d77d3877a7189426973
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2026-05-21 16:38:03 +0200

    fix img optimalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [react()],
   output: "static",
   site: "https://dani.pw",
-  adapter: cloudflare(),
+  adapter: cloudflare({ imageService: "compile" }),
   vite: {
     plugins: [tailwindcss()],
   },
