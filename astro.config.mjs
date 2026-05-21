21f8f41c7351f824d121b629f3e4977d5e14fecd
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2026-05-21 15:58:39 +0200

    use astro font, add preload, update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
@@ -11,7 +11,42 @@ export default defineConfig({
   output: "static",
   site: "https://dani.pw",
   vite: {
-    // @ts-expect-error astro types
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "SF Pro Display",
+      cssVariable: "--font-sf-pro",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/SF-Pro-Display-Light.otf"],
+            weight: "300",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SF-Pro-Display-Regular.otf"],
+            weight: "400",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SF-Pro-Display-Medium.otf"],
+            weight: "500",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SF-Pro-Display-Semibold.otf"],
+            weight: "600",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/SF-Pro-Display-Bold.otf"],
+            weight: "700",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
