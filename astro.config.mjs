cf52c35f55a05ab83012bc5b445630cd8d32eec0
Daniel Pallares <34248806+1danii@users.noreply.github.com>
2026-05-21 16:32:59 +0200

    migrate to workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,18 @@ import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "static",
   site: "https://dani.pw",
+  adapter: cloudflare(),
   vite: {
-    // @ts-expect-error
     plugins: [tailwindcss()],
   },
+
   fonts: [
     {
       provider: fontProviders.local(),
